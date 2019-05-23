94f7f6460544606d9756cfdcb69396e016626daf
ErikaLFrost <erika.loman@gmail.com>
2019-05-23 10:41:27 +0200

    tagit bort kommentarer

--- src/routes/login.php
+++ src/routes/login.php
@@ -3,7 +3,7 @@
 return function ($app) {
   // Register auth middleware
   $auth = require __DIR__ . '/../middlewares/auth.php';
-/*
+
    // Add a login route
    $app->post('/api/login', function ($request, $response) {
      $data = $request->getParsedBody();
@@ -39,7 +39,7 @@ return function ($app) {
       //}
       //});
 
-
+/*
   $app->post('/api/login', function ($request, $response) {
     $data = $request->getParsedBody();
     if ($data['username'] && $data['password']) {
@@ -52,7 +52,7 @@ return function ($app) {
       return $response->withStatus(401);
     }
   });
-  
+  */
 
   // Add a ping route
   $app->get('/api/ping', function ($request, $response, $args) {
